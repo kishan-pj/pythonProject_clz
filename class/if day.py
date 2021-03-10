9e5006d9686b2fad2c74c6e0fe67ce6b77d1cb88
Kishan Prasad Jaiswal <bhootk123@gmail.com>
2021-03-10 08:00:43 +0545

    first commit

---
+++ class/if day.py
@@ -0,0 +1,16 @@
+num=int(input("enter the num"))
+if num==1:
+    print("the day is sunday")
+elif  num==2:
+    print("the is monday")
+elif num==3:
+    print("the day is tuesday")
+elif num==4:
+    print("the day is wednesday")
+elif num==5:
+    print("the day is thursday")
+elif num==6:
+    print("the day is friday")
+elif num==7:
+    print("the day is saturday")
+print("the program is over")
