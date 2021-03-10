7d09016f5df1a4d6f12f810ceeaa1bca7ad61373
Kishan Prasad Jaiswal <bhootk123@gmail.com>
2021-03-10 07:49:02 +0545

    444

---
+++ class/if condition.py
@@ -0,0 +1,23 @@
+num=int(input("Enter the number:"))
+if num%2 ==0:
+    print("the number is even")
+else:
+    print("the number is odd")
+print("the program is over")
+
+num=int(input("enter the number:"))
+if num>0:
+    print("the number is positive")
+else:
+    print("the number is negative")
+print("the program is over")
+
+
+num=int(input("enter the number:"))
+if num==0:
+    print("the number is zero")
+elif num>0:
+    print("the number is positive")
+else:
+    print("the number is negative ")
+print("the number is over")
